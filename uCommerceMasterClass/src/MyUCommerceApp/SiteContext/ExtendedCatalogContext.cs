9b7bc3617f2c345169739932a64205a53fe4feec
Maxim Zhigulsky <insolent.1985@gmail.com>
2017-06-15 23:33:33 +0300

    Part 3. Components. Custom implementations

---
+++ uCommerceMasterClass/src/MyUCommerceApp/SiteContext/ExtendedCatalogContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using UCommerce.Content;
+using UCommerce.EntitiesV2;
+using UCommerce.Runtime;
+
+namespace MyUCommerceApp.BusinessLogic.SiteContext
+{
+    class ExtendedCatalogContext : CatalogContext
+    {
+        public ExtendedCatalogContext(IDomainService domainService,
+            IRepository<ProductCatalogGroup> productCatalogGroupRepository,
+            IRepository<ProductCatalog> productCatalogRepository,
+            IRepository<PriceGroup> priceGroupRepository) 
+            : base(domainService, productCatalogGroupRepository, productCatalogRepository, priceGroupRepository)
+        {
+
+        }
+
+        public override string CurrentCatalogName
+        {
+            get
+            {
+                if (UserIsLogginIn())
+                    return "Private";
+                return base.CurrentCatalogName;
+            }
+
+            set
+            {
+                base.CurrentCatalogName = value;
+            }
+        }
+
+        private bool UserIsLogginIn()
+        {
+            //need to be in UMBRAOC development environment to imlement this logic
+            return HttpContext.Current.Request.QueryString["IsLoggedIn"] != null;
+            
+        }
+    }
+}
