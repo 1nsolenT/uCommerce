e555f34a8d039a3bc9d2e6ef2543d39156713a15
Maxim Zhigulsky <insolent.1985@gmail.com>
2017-06-15 12:42:36 +0300

    Part 3

--- uCommerceMasterClass/src/MyUCommerceApp.Runner/Program.cs
+++ uCommerceMasterClass/src/MyUCommerceApp.Runner/Program.cs
@@ -5,6 +5,7 @@ using NHibernate.Linq;
 using UCommerce.EntitiesV2;
 using UCommerce.Infrastructure;
 using UCommerce.Search.RavenDB;
+using NHibernate;
 
 namespace MyUCommerceApp.Integration
 {
@@ -12,7 +13,65 @@ namespace MyUCommerceApp.Integration
 	{
 		static void Main(string[] args)
 		{
-            var firstOrder = PurchaseOrder.All().First();
+            var firstOrder = PurchaseOrder
+                .All()
+                .First();
+            
+            var repo = ObjectFactory.Instance.Resolve<IRepository<PurchaseOrder>>();
+            firstOrder = repo
+                .Select()  // incapsulate provider.GetSession().Query<PurchaseOrder>();
+                .First(i => i.Shipments.Any());
+            
+            ISessionProvider provider = ObjectFactory.Instance.Resolve<ISessionProvider>();
+            ISession session = provider.GetSession();
+            firstOrder = session
+                .Query<PurchaseOrder>()
+                .First();
+
+            var ordersByCetainFromStaticLayer = PurchaseOrder.All().Where(i =>
+                i.CompletedDate > new DateTime(2009, 1, 1)).ToList();
+
+
+
+
+
+            var productRepository = ObjectFactory.Instance.Resolve<IRepository<Product>>();
+            var productOnHomePage = productRepository.Select(i => i.ProductProperties.Any(
+                y => y.ProductDefinitionField.Name == "ShowOnHomepage" &&
+                y.Value == "True"
+                )).ToList();
+
+            productOnHomePage = productRepository.Select(
+                new ProductsByPropertiesQuery("ShowOnHomePage", "True")).ToList();
+
+
+
+
+
+            var provider_1 = ObjectFactory.Instance.Resolve<ISessionProvider>();
+            using (var session_1 = provider_1.GetSession())
+            {
+                var orders = session_1.Query<PurchaseOrder>().Fetch(i=>i.Customer).ToList();
+                foreach (var order in orders)
+                {
+                    if (order.Customer != null)
+                    {
+                        Console.WriteLine(order.Customer.FirstName);
+                    }
+                }
+            }
+            Console.ReadLine();
+
+
+            var provide_2 = ObjectFactory.Instance.Resolve<ISessionProvider>();
+            provide_2.GetSession().Query<Product>().Where(i => i.ProductId == 105)
+                .FetchMany(x => x.Variants) //4
+                .FetchMany(x => x.CategoryProductRelations) //4
+                .FetchMany(x => x.ProductRelations) //4
+                .ToList(); // total 4*4*4 be aware
+            
+
+
         }
 	}
 }
---
+++ uCommerceMasterClass/src/MyUCommerceApp/Queries/ProductsByPropertiesQuery.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using UCommerce.EntitiesV2.Queries;
+using UCommerce.EntitiesV2;
+using NHibernate.Linq;
+
+namespace MyUCommerceApp.BusinessLogic.Queries
+{
+    public class ProductsByPropertiesQuery : ICannedQuery<Product>
+    {
+        //public void Execute(ISession session)
+        //{
+        //    
+        //}
+        string _fieldName;
+        string _propertyName;
+        public ProductsByPropertiesQuery(string fieldName, string propertyValue)
+        {
+            this._fieldName = fieldName;
+            this._propertyName = propertyValue;
+        }
+
+        public IEnumerable<Product> Execute(ISession session)
+        {
+            return session.Query<Product>()
+                .Where(i =>
+                    i.ProductProperties.Any(y =>
+                    y.ProductDefinitionField.Name == _fieldName && y.Value == _propertyName));
+        }
+
+        //IEnumerable<T> ICannedQuery<T>.Execute(ISession session)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
