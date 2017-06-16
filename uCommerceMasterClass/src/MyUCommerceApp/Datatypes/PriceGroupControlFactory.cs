8c16351ae81a086189ee31a87e4e1382f5e06e9b
Maxim Zhigulsky <insolent.1985@gmail.com>
2017-06-16 12:22:07 +0300

    part 4 in progress

---
+++ uCommerceMasterClass/src/MyUCommerceApp/Datatypes/PriceGroupControlFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UCommerce.EntitiesV2;
+using UCommerce.EntitiesV2.Definitions;
+using UCommerce.Presentation.Web.Controls;
+
+namespace MyUCommerceApp.BusinessLogic.Datatypes
+{
+    public class PriceGroupControlFactory : IControlFactory
+    {
+        public bool Supports(DataType dataType)
+        {
+            return dataType.DefinitionName == "PriceGroup";
+        }
+
+        public Control GetControl(IProperty property)
+        {
+            var dropdownList = new SafeDropDownList();
+            var priceGroups = PriceGroup.All().ToList();
+            foreach (var item in priceGroups)
+            {
+                var listItem = new ListItem(item.Name, item.PriceGroupId.ToString());
+                listItem.Selected = property.GetValue().ToString() == item.PriceGroupId.ToString();
+                dropdownList.Items.Add(listItem);
+            }
+            return dropdownList;
+        }
+    }
+}
---
+++ uCommerceMasterClass/src/MyUCommerceApp/Pipelines/ExportOrderToErpSystemTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UCommerce;
+using UCommerce.EntitiesV2;
+using UCommerce.Pipelines;
+
+namespace MyUCommerceApp.BusinessLogic.Pipelines
+{
+    public class ExportOrderToErpSystemTask : IPipelineTask<PurchaseOrder>, IUndoablePipelineTask<PurchaseOrder>
+    {
+        public PipelineExecutionResult Execute(PurchaseOrder subject)
+        {
+            string oderInformation = string.Format("{0}:{1} customer: {2}\r\n",
+                subject.OrderNumber,
+                new Money(subject.OrderTotal.GetValueOrDefault(), subject.BillingCurrency).ToString(),
+                subject.Customer.EmailAddress);
+            File.AppendAllText("C:\\Test\\Erp.txt", oderInformation);
+
+            return PipelineExecutionResult.Success;
+        }
+
+        public PipelineExecutionResult Undo(PurchaseOrder subject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
